b29cfa608dc8379e03d1a4fb8190550e9c20cb88
mohamed <ghodhban.amine@gmail.com>
2019-04-07 09:49:26 +0200

    edit files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ffmpeg_scripts
+
+This reposity is a series of script related to ffmpeg courses.
+Each script inclue ffmpeg command lines
+
+Please, be sure to place the scritp in the same folder of input files.
+
+sample :
+chmod +x script_demo.sh
+./script_demo.sh input.mp4
+
--- script_demo.sh
+++ script_demo.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+#variable list
+bitrate1=1000k
+bitrate2=1000k
+bitrate3=1000k
 
 
-ffmpeg $1 -bitrate 5000k output_5K.mp4
-ffmpeg $1 -bitrate 3000k output_3K.mp4
-ffmpeg $1 -bitrate 1000k output_1K.mp4
+#command line
+# "$1" is the input file
+# ./script_demo.sh input.mp4
+ 
+ffmpeg $1 -bitrate $bitrate1 output_1K.mp4
+
+ffmpeg $1 -bitrate $bitrate1 output_3K.mp4
+
+ffmpeg $1 -bitrate $bitrate1 output_5K.mp4
